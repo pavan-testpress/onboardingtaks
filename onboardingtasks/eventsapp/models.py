404880fad570b3b9373fcb460d89f0803caa8983
Pavan Kumar <Pavan@testpress.in>
2018-12-19 18:16:10 +0530

    Add Authentication to Eventsapp
    - Change Eventsapp to use authentication app user authentication
    - Block signup, login url's after user login
    - Redirect event creation url to login page if user not authenticated

--- onboardingtasks/eventsapp/models.py
+++ onboardingtasks/eventsapp/models.py
@@ -4,18 +4,20 @@ from django.db import models
 from taggit.managers import TaggableManager
 # Create your models here.
 
+
 class Events(TimeStampedModel):
     title = models.CharField(max_length = 100)
-    place = models.ForeignKey(Places,on_delete = models.CASCADE)
+    place = models.ForeignKey(Places, on_delete = models.CASCADE)
     tags = TaggableManager()
     created_by = models.CharField(max_length=50)
 
-    def __str__(slef):
+    def __str__(self):
         return self.title
+
 
 class EventTime(models.Model):
     event_start_time = models.DateTimeField(auto_now=False, auto_now_add=False)
     event_end_time = models.DateTimeField(auto_now=False, auto_now_add=False)
-    event = models.ForeignKey(Events,on_delete = models.CASCADE,related_name="event_times")
+    event = models.ForeignKey(Events, on_delete=models.CASCADE, related_name="event_times")
 
 
--- onboardingtasks/eventsapp/views.py
+++ onboardingtasks/eventsapp/views.py
@@ -1,46 +1,56 @@
-from django.shortcuts import render
-from django.views.generic import ListView,CreateView
-from eventsapp.models import Events,EventTime
-from eventsapp.forms import EventForm,EventTimeForm
-from django.forms import formset_factory
+from django.shortcuts import render, reverse
+from django.views.generic import ListView, CreateView
+from .models import Events, EventTime
+from .forms import EventForm, EventTimeForm
 from datetime import datetime
+from django.http import HttpResponseRedirect
+
+
 # Create your views here.
 
 
 class EventsListView(ListView):
     model = Events
     template_name = "eventsapp/list.html"
-    def get(self,request):
-        events = self.get_queryset(request)
-        return render(request,self.template_name,{'events':events})
-    def get_queryset(self,request):
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            events = self.get_queryset(request)
+            return render(request, self.template_name, {'events': events})
+        else:
+            return HttpResponseRedirect(reverse('authenticationapp:login'))
+
+    def get_queryset(self, request):
         qs = super().get_queryset()
         results = set()
-        if('weekday' in request.GET):
+        if 'weekday' in request.GET:
             for q in qs:
-                c_all =q.event_times.all()
+                c_all = q.event_times.all()
                 for c in c_all:
                     if str(c.event_start_time.weekday()) == (request.GET['weekday']):
                         results.add(q)
             return results
-        elif('yyyy-mm' in request.GET):
-            year,month = request.GET['yyyy-mm'].split('-')
-            qs = set(qs.filter(event_times__event_start_time__year = year,event_times__event_start_time__month=month))
-        elif('yyyy-mm-dd' in request.GET):
-            year,month,date = request.GET['yyyy-mm-dd'].split('-')
-            qs = set(qs.filter(event_times__event_start_time__year = year,event_times__event_start_time__month=month,event_times__event_start_time__day=date))
+        elif 'yyyy-mm' in request.GET:
+            year, month = request.GET['yyyy-mm'].split('-')
+            qs = set(qs.filter(event_times__event_start_time__year=year, event_times__event_start_time__month=month))
+        elif 'yyyy-mm-dd' in request.GET:
+            year, month, date = request.GET['yyyy-mm-dd'].split('-')
+            qs = set(qs.filter(event_times__event_start_time__year=year, event_times__event_start_time__month=month,
+                               event_times__event_start_time__day=date))
         return qs
 
 
 class EventsCreateView(CreateView):
-    def get(self,request):
-        eform = EventForm()
-        #tform = formset_factory(EventTimeForm, extra=1)
-        tform = EventTimeForm()
-        template_name = "eventsapp/createview.html"
-        return render(request,template_name,{'eform':eform,'tform':tform,}) 
+    def get(self, request):
+        if request.user.is_authenticated:
+            eform = EventForm()
+            tform = EventTimeForm()
+            template_name = "eventsapp/createview.html"
+            return render(request, template_name, {'eform': eform, 'tform': tform, })
+        else:
+            return HttpResponseRedirect(reverse('authenticationapp:login'))
 
-    def post(self,request):
+    def post(self, request):
         template_name = "eventsapp/createview.html"
         eform = EventForm(request.POST)
         if eform.is_valid():
@@ -50,12 +60,12 @@ class EventsCreateView(CreateView):
             eform.created_by = eform.cleaned_data['created_by']
             e = eform.save()
         tform = EventTime()
-        event_start_time = request.POST['event_start_time_0']+" "+request.POST['event_start_time_1']
-        event_start_time = datetime.strptime(event_start_time, '%Y-%m-%d %H:%M:%S') 
+        event_start_time = request.POST['event_start_time_0'] + " " + request.POST['event_start_time_1']
+        event_start_time = datetime.strptime(event_start_time, '%Y-%m-%d %H:%M:%S')
         tform.event_start_time = event_start_time
-        event_end_time = request.POST['event_end_time_0']+" "+request.POST['event_end_time_1']
-        event_end_time = datetime.strptime(event_end_time, '%Y-%m-%d %H:%M:%S') 
+        event_end_time = request.POST['event_end_time_0'] + " " + request.POST['event_end_time_1']
+        event_end_time = datetime.strptime(event_end_time, '%Y-%m-%d %H:%M:%S')
         tform.event_end_time = event_end_time
-        tform.event = Events.objects.get(pk =e.pk)
+        tform.event = Events.objects.get(pk=e.pk)
         tform.save()
-        return render(request,template_name,{}) 
+        return render(request, template_name, {})
