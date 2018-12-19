aeb501fa9912f8a437ce68ffdb9c120a8d891630
Pavan Kumar <Pavan@testpress.in>
2018-12-19 19:01:37 +0530

    Add Authentication to Pages app
    - Use Authentication from Authentication app to open app
    - Override dispatch method of all apps to authenticate

--- onboardingtasks/pagesapp/views.py
+++ onboardingtasks/pagesapp/views.py
@@ -1,32 +1,46 @@
-from django.shortcuts import render
-from django.conf.urls import url
+from django.http import HttpResponseRedirect
+from django.shortcuts import render,reverse
 from django.views.generic import TemplateView
-from pagesapp.forms import PagesForm
-from pagesapp.models import Pages
+from .forms import PagesForm
+from .models import Pages
 
 
 class HomePageView(TemplateView):
     template_name = 'pagesapp/home.html'
 
-    def get(self,request):
-        data = Pages.objects.all().order_by('ordering','-modified')
+    def dispatch(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return super(HomePageView, self).dispatch(*args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('authenticationapp:login'))
+
+    def get(self, request):
+        data = Pages.objects.all().order_by('ordering', '-modified')
         form = PagesForm()
-        return render(request,self.template_name,{'form':form,'data':data})
+        return render(request, self.template_name, {'form': form, 'data': data})
     
-    def post(self,request):
+    def post(self, request):
         data = Pages.objects.all().order_by('ordering','-modified')
         form = PagesForm(request.POST)
         if form.is_valid:
             form.save()
         form = PagesForm()
-        return render(request,self.template_name,{'form':form,'data':data})
+        return render(request, self.template_name, {'form': form, 'data': data})
+
 
 class NavPageView(TemplateView):
     template_name = 'pagesapp/navpages.html'
-    def get(self,request):
+
+    def dispatch(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return super(NavPageView, self).dispatch(*args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('authenticationapp:login'))
+
+    def get(self, request):
         pat=request.path.split('/')[2]
         h = Pages.objects.get(slug=pat)
         html = h.content_html
         title = h.title
-        data = Pages.objects.all().order_by('ordering','-modified')
-        return render(request,self.template_name,{'data':data,'html':html,'title':title})
+        data = Pages.objects.all().order_by('ordering', '-modified')
+        return render(request, self.template_name, {'data': data, 'html': html, 'title': title})
